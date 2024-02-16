94526a5751d8db15b3f90b4097da12fa123b066b
yuekcc <yuekcc@qq.com>
2024-02-16 23:11:29 +0800

    refactor

--- src/use-monaco-editor.js
+++ src/use-monaco-editor.js
@@ -1,13 +1,14 @@
 import * as monaco from 'monaco-editor-core';
 
 const MonacoEnvironment = 'MonacoEnvironment';
+const WORKER_BASE_URL = '/dist';
 
 function setupWorkerConfig() {
   window[MonacoEnvironment] = {
     getWorkerUrl(moduleId, label) {
       console.log('#getWorkerUrl', moduleId, label);
       if (moduleId === 'workerMain.js') {
-        return '/dist/monaco-editor.worker.js';
+        return `${WORKER_BASE_URL}/monaco-editor.worker.js`;
       }
 
       return '';
